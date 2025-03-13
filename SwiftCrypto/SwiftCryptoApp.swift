15fc361f6e041cb725edc784e95f81e2e8985c5c
hassamshaikh <hassamshaikh94@gmail.com>
2025-03-13 11:50:27 +0500

    project initialized and add custom color

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Hassam Shaikh on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
